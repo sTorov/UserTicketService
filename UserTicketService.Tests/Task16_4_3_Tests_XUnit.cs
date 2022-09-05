28ac85fc632de1cefd48b4f9b076299ba4ccd673
sTorov <sotorov2011@yandex.ru>
2022-09-05 23:23:47 +0300

    Task16.4.3

---
+++ UserTicketService.Tests/Task16_4_3_Tests_XUnit.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+
+namespace UserTicketService.Tests
+{
+
+    /*
+     * Программист начал разрабатывать приложение Calculator при помощи подхода TDD. Он написал первые тесты для будущего класса
+     * Реализуйте код класса Calculator, который успешно пройдет данные тесты.
+     */
+
+    public class Task16_4_3_Tests_XUnit //CalculatorTests
+    {
+        [Fact]
+        public void AdditionalMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            int result = calculator.Addition(50, 10, 34);
+            Assert.Equal(94, result);
+        }
+
+        [Fact]
+        public void MultiplicationMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            int result = calculator.Multiplication(3, 2, 20);
+            Assert.Equal(120, result);
+        }
+    }
+}
---
+++ UserTicketService/Task16_4_3_Calculator.cs
@@ -0,0 +1,15 @@
+﻿namespace UserTicketService
+{
+    public class Calculator
+    {
+        public int Addition(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        public int Multiplication(int a, int b, int c)
+        {
+            return a * b * c;
+        }
+    }
+}
