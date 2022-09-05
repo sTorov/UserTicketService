309d4ad765be3c9e677bd404ccb93dc35b61499d
sTorov <sotorov2011@yandex.ru>
2022-09-05 23:14:30 +0300

    TDD Translator

---
+++ Translator.Tests/TranslateServiceTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace Translator.Tests
+{
+    [TestFixture]
+    public class TranslateServiceTests
+    {
+        [Test]
+        public void GetTranslation_MustReturnCorrectValue()
+        {
+            var translateService = new TranslateService();
+            translateService.Add("orange", "апельсин");
+
+            Assert.AreEqual("апельсин", translateService.GetTranslation("orange"));
+        }
+    }
+}
---
+++ Translator/TranslateService.cs
@@ -0,0 +1,24 @@
+﻿namespace Translator
+{
+    public class TranslateService
+    {
+        Dictionary<string, string> _translations;
+
+        public TranslateService()
+        {
+            _translations = new Dictionary<string, string>();
+            _translations.Add("dog", "собака");
+            _translations.Add("cat", "кошка");
+        }
+
+        public void Add(string oneWord, string twoWord)
+        {
+            _translations.Add(oneWord, twoWord);
+        }
+
+        public string GetTranslation(string word)
+        {
+            return _translations[word];
+        }
+    }
+}
