06b2507ef5f26c72f23a97880cbed3ae066095b2
sTorov <sotorov2011@yandex.ru>
2022-09-05 05:31:47 +0300

    Задание 16.2.6
    Напишите тест для функции Division, проверяющий исключение DivideByZeroException при попытке деления числа на ноль. При написании теста используйте фреймворк NUnit.

--- UserTicketService.Tests/CalculationTests.cs
+++ UserTicketService.Tests/CalculationTests.cs
@@ -13,7 +13,7 @@ namespace UserTicketService.Tests__NUnit_
             Assert.AreEqual(8, calculation.Multiplication(2, 4));
         }
 
-        //Задание 16.2.4
+        //Задание 16.2.4 (Проверка корректности вычислений)
         [Test]
         public void Subtract_MustReturnCorrectValue()
         {
@@ -22,13 +22,31 @@ namespace UserTicketService.Tests__NUnit_
             Assert.That(0 == calculation.Subtraction(1, 1));
         }
 
-        //Задание 16.2.5
+        //Задание 16.2.5 (Проверка корректности вычислений)
         [Test]
         public void Division_MustReturnCorrectValue()
         {
             Calculation calculation = new();
 
             Assert.That(5 == calculation.Division(25, 5));
+        }
+
+        //Задание 16.2.6 (Проверка типа исключения)
+        [Test]
+        public void Division_MustReturnThrowException()
+        {
+            var calculation = new Calculation();
+
+            Assert.Throws<DivideByZeroException>(() => calculation.Division(5, 0));
+
+            //try
+            //{
+            //    calculation.Division(10, 0);
+            //}
+            //catch (Exception e)
+            //{
+            //    Assert.IsTrue(e is DivideByZeroException);
+            //}
         }
     }
 }
