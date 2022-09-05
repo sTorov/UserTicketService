a45d1188832d1c67d113fc48a2bf59c4bd899524
sTorov <sotorov2011@yandex.ru>
2022-09-05 22:28:38 +0300

    Task16.3.2

---
+++ UserTicketService.Tests/Task16_3_2_Tests_XUnit.cs
@@ -0,0 +1,24 @@
+﻿using Moq;
+using Xunit;
+
+namespace UserTicketService.Tests
+{
+    public class Task16_3_2_Tests_XUnit
+    {
+        [Fact]
+        public void FindAll_MustReturnCorrectValue()
+        {
+            var mock = new Mock<IBookRepository>();
+            mock.Setup(x => x.FindAll()).Returns(new List<Book>
+            {
+                new Book { Title = "Война и мир" },
+                new Book { Title = "Человек-невидимка" },
+                new Book { Title = "Анна Каренина" },
+            });
+
+            Assert.Contains(mock.Object.FindAll(), x => x.Title == "Война и мир");
+            Assert.Contains(mock.Object.FindAll(), x => x.Title == "Человек-невидимка");
+            Assert.Contains(mock.Object.FindAll(), x => x.Title == "Анна Каренина");
+        }
+    }
+}
---
+++ UserTicketService/Task16_3_2.cs
@@ -0,0 +1,28 @@
+﻿namespace UserTicketService
+{
+    /*
+     * По аналогии с предыдущим заданием выполните тестирование функции FindAll при помощи moq-объекта. 
+     * Используйте следующие три названия книг для добавления в moq-коллекцию: 
+     * Война и мир, Человек-невидимка, Анна Каренина. При написании теста используйте фреймворк XUnit. 
+     * Для проверки на существование элемента в коллекции используйте метод Contains класса Assert.
+     */
+
+    public class BookRepository : IBookRepository
+    {
+        public IEnumerable<Book> FindAll()
+        {
+            return null;
+        }
+    }
+
+    public interface IBookRepository
+    {
+        IEnumerable<Book> FindAll();
+    }
+
+    public class Book
+    {
+        public string Title
+        { get; set; }
+    }
+}
