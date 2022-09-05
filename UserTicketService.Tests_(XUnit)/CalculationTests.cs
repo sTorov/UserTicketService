d1473e6022eab1d8f398a201acac58137ba1a7d6
sTorov <sotorov2011@yandex.ru>
2022-09-05 05:36:53 +0300

    Задание 16.2.7
    Напишите тест для функции Subtraction. При написании теста используйте фреймворк XUnit.

--- UserTicketService.Tests_(XUnit)/CalculationTests.cs
+++ UserTicketService.Tests_(XUnit)/CalculationTests.cs
@@ -5,11 +5,20 @@ namespace UserTicketService.Tests__XUnit_
     public class CalculationTests
     {
         [Fact]
-        public void MultiplicationMustReturnNotNull()
+        public void Multiplication_MustReturnNotNull()
         {
             Calculation calculation = new Calculation();
 
             Assert.Equal(8, calculation.Multiplication(4, 2));
+        }
+
+        //Задание 16.2.7 (Проверка корректности вычислений)
+        [Fact]
+        public void Subtraction_MustReturnCorrectValue()
+        {
+            var calculation = new Calculation();
+
+            Assert.True(5 == calculation.Subtraction(10, 5));
         }
     }
 }
