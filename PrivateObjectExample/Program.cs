86046636e347d63fc05a858c1c59b4f20a07d47c
sTorov <sotorov2011@yandex.ru>
2022-09-07 18:39:17 +0300

    Update PrivateObjectExample

--- PrivateObjectExample/Program.cs
+++ PrivateObjectExample/Program.cs
@@ -1,7 +1,34 @@
-﻿class Program
+﻿using PrivateObjectExample;
+using System.Reflection;
+using System.Text;
+
+/*
+ * Приватные члены класса не наследуются
+ */
+
+class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.OutputEncoding = Encoding.UTF8;
+
+        Type typeMyClass = typeof(MyClass);
+        Type typeBaseClass = typeof(BaseClass);
+        BindingFlags bindingFlags = BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic;
+
+        var myClassMethods = typeMyClass.GetMethods(bindingFlags);
+        var baseClassMethods = typeBaseClass.GetMethods(bindingFlags);
+
+        Console.WriteLine("MyClass:\n");
+        foreach(var method in myClassMethods)
+            Console.WriteLine(method.Name);
+
+        Console.WriteLine();
+
+        Console.WriteLine("BaseClass:\n");
+        foreach (var method in baseClassMethods)
+            Console.WriteLine(method.Name);
+
+        Console.ReadKey();
     }
 }
